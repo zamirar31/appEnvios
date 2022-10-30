f8a4330fe30592ae3abdbcf4f3e8f8f60dd80d9b
zamirar31 <101940952+zamirar31@users.noreply.github.com>
2022-10-30 13:47:03 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 require('dotenv').config()
+const Server = require('./models/server')
+
+const server = new Server();
+server.listen();
+
 const express = require('express')
 const app = express()
 app.get('/', function(req, res){
